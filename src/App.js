dcd6ff423052ba46e11fe6749b18bfe7ad375f42
DESKTOP-5GCHDV4\mnk20 <johnyvesmarin@gmail.com>
2020-06-21 12:01:41 +0800

    change css and add p

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>John Yves Marin</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit and Save <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
